faf5f4c8b5424d95c1cef13d2654562491b610ef
Chen Stan <stan.chen@ssbshoes.com>
2023-12-14 09:06:25 +0700

    Report bug fix

--- CMS-API/Controllers/CMSController.cs
+++ CMS-API/Controllers/CMSController.cs
@@ -215,7 +215,7 @@ namespace API.Controllers
 
             if (sCarManageRecordDto.SignInDateS == "" || sCarManageRecordDto.SignInDateS == null) sCarManageRecordDto.SignInDateS = _config.GetSection("LogicSettings:MinDate").Value;
             if (sCarManageRecordDto.SignInDateE == "" || sCarManageRecordDto.SignInDateE == null) sCarManageRecordDto.SignInDateE = _config.GetSection("LogicSettings:MaxDate").Value;
-            sCarManageRecordDto.SignInDateE = sCarManageRecordDto.SignInDateE.ToDateTime().AddDays(1).ToString().Substring(0, 9).Replace('/', '-');
+            sCarManageRecordDto.SignInDateE = sCarManageRecordDto.SignInDateE.ToDateTime().AddDays(1).ToString("yyyy-MM-dd").Substring(0, 10).Replace('/', '-');
 
             var data = await _cMSCarManageRecordDAO.GetCarManageRecordDto(sCarManageRecordDto);
 
